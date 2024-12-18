913b0871a22e60cc76c9522af01e63aca266f0f3
NikhilChoukikar <91651344+NikhilChoukikar@users.noreply.github.com>
2024-12-18 17:59:51 +0000

    Datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,21 @@
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint
+// string = ""
+// boolean = true/false
+// null = standalone value, Null is a special type
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof null )
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) // object
+
+// Null is an object type
+// Undefined as undefined type
