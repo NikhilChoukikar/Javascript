53f1a807ba9814d3905d4b2bbe539a576e41e859
NikhilChoukikar <91651344+NikhilChoukikar@users.noreply.github.com>
2024-12-21 08:25:43 +0000

    Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,82 @@
+// let score = 33
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let score1 = "33"
+
+// console.log(typeof score1)
+// console.log(typeof (score1))
+
+// let ValueinNumber = Number(score)
+// console.log(typeof ValueinNumber)
+
+// let score2 = "33abc"
+
+// let ValueinNumber = Number(score2)
+// console.log(typeof ValueinNumber)
+// console.log(ValueinNumber)
+
+// let score3 = null 
+
+// console.log(score3)
+// console.log(typeof score3)
+
+//  let ValueinNumber = Number(score3)
+//  console.log(typeof ValueinNumber)
+// console.log(ValueinNumber)
+
+// let score4 = undefined 
+
+// console.log(score4)
+// console.log(typeof score4)
+
+//  let ValueinNumber1 = Number(score4)
+//  console.log(typeof ValueinNumber1)
+// console.log(ValueinNumber1)
+
+// let score5 = true
+
+// console.log(score5)
+// console.log(typeof score5)
+
+//  let ValueinNumber2 = Number(score5)
+//  console.log(typeof ValueinNumber2)
+// console.log(ValueinNumber2)
+
+// let score6 = "Nikhil"
+
+// console.log(score6)
+// console.log(typeof score6)
+
+//  let ValueinNumber3 = Number(score6)
+//  console.log(typeof ValueinNumber3)
+// console.log(ValueinNumber3)
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1; false =>0;
+// Null = 0;
+
+// let isLoggedIn = Null
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+// 1 => true ; 0 => false
+// "" => false
+// "Nikhil" = true
+// Null = Null is not defined
+
+// let someNumber = "qwer"
+
+// let stringNumber = String(someNumber)
+
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// qwer = string
+// 123 = string
+// Null = string
+// 0 = string
