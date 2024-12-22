3b7a0932ec9156c6ab0399fcaa5792d5bf4b3e7d
NikhilChoukikar <91651344+NikhilChoukikar@users.noreply.github.com>
2024-12-22 11:19:14 +0000

    Summary.js

---
+++ 01_basics/05_summary.js
@@ -0,0 +1,44 @@
+// Primitive 
+
+// 7 types : String, number,Boolean, Null, Undefined, Symbol, BigInt
+
+// Reference( Non-primitive)
+
+// Array, Objects, Functions
+
+// const score = 100
+
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+
+// const id = Symbol("123")
+// const anotherId = Symbol("123")
+
+// console.log(typeof id)
+// console.log(typeof anotherId)
+
+// const BigNumber = 12345686549876543287654876543654333333333333n
+
+// console.log( typeof BigNumber)
+
+// const heroes = ["Shaktimaan","Nagraj","Doga"];
+
+// let myObj = {
+//     name: "Hitesh",
+//     age : 22,
+// }
+
+// const MyFunction = function(){
+//     console.log("Hello World")
+// }
+
+// console.log(typeof MyFunction)
+// console.log(typeof myObj)
+// console.log(typeof heroes)
+
+// Array - Object
+// Objects- Object
+// Functions- Object Function
