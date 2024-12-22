20cda9706957056506421e300642f4036457ddae
NikhilChoukikar <91651344+NikhilChoukikar@users.noreply.github.com>
2024-12-22 14:27:30 +0000

    Strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,38 @@
+// const name = "Nikhil"
+// const repoCount = 50
+
+// console.log(name + repoCount +" Value")
+
+// console.log(`Hello My name is ${name} my repo count is ${repoCount}`);
+
+// const gameName = new String('hiteshhc')
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('e'))
+
+// const newstring = gameName.substring(0,4)
+// console.log(newstring)
+
+// const anotherstring = gameName.slice(0,4)
+// console.log(anotherstring);
+
+// const anotherstring1 = gameName.slice(-8,4)
+// console.log(anotherstring1)
+
+// const newStringOne = "  Nikhil  "
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
+
+// const Url = "https://Nikhil.com//nikhil%20choukikar"
+
+// console.log(Url.replace('%20','-'))
+// console.log(Url.includes('sundar'))
+
+// const gameName1 = String("Nikhil-n-c")
+// console.log(gameName1.split('-'))
+
