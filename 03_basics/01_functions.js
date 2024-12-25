33b7fef187cb98087a2983d66220b01944da59a6
NikhilChoukikar <91651344+NikhilChoukikar@users.noreply.github.com>
2024-12-25 15:40:22 +0000

    rest and spread operator

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -11,29 +11,61 @@
 
 // function addTwoNumbers(number1, number2){
 
-//     console.log(number1 + number2);             //Console does not create a result, we have to create a return type to get result. The type of console log is undefined that's why we use return.
+//     console.log(number1 + number2);             
 // }
 
 
-function addTwoNumbers(number1, number2){
+// function addTwoNumbers(number1, number2){
 
-    // let result = number1 + number2
-    // return result
-    return number1 + number2
-}
+//     // let result = number1 + number2
+//     // return result
+//     return number1 + number2
+// }
 
-const result = addTwoNumbers(3, 5)
+// const result = addTwoNumbers(3, 5)
 
- console.log("Result: ", result);
+//  console.log("Result: ", result);            //Console does not create a result, we have to create a return type to get result. The type of console log is undefined that's why we use return.
 
 
-function loginUserMessage(username = "sam"){
-    if(!username){
-        console.log("PLease enter a username");
-        return
-    }
-    return `${username} just logged in`
-}
+// function loginUserMessage(username = "sam"){
+//     if(!username){
+//         console.log("PLease enter a username");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
 
 // console.log(loginUserMessage("hitesh"))
 // console.log(loginUserMessage("hitesh"))
+
+
+
+// function calculateCartPrice(val1,val2,...num1){
+//     return num1
+// }
+
+//  console.log(calculateCartPrice(200,400,500,2000))
+
+//  const user = {
+//     username: "Nikhil",
+//     prices: 199
+//  }
+
+//  function HandleObject(anyObject){
+//     console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`)
+//  }
+
+//  // handleObject(user)
+// HandleObject({                             // Handle object can also be called this way
+//     username:"sam",
+//     price: 199
+// })
+
+// const myNewArray = [200,400,100,600]
+
+// function returnSecondValue(getArray){
+//     return getArray[1]
+// }
+
+// console.log(returnSecondValue(myNewArray[2]))
+// console.log(returnSecondValue([200,400,100,600]))
