a49ddbc0b87d00f7df64c2aaabbe2e5ba9d1c087
NikhilChoukikar <91651344+NikhilChoukikar@users.noreply.github.com>
2024-12-26 15:20:49 +0000

    scopes in objects and functions

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,21 +1,59 @@
 // var c = 300
-let a = 300
-                                 // Don't use var because its value is globally changing, if the value is changed in one file, then its value is changed in other file. 
-                                 // Blockscope - Value within the block Example within if statement
-                                 // Globalscope - Everything else, except block
-if(true){
-    let a = 10
-    const b = 20
+// let a = 300
+//                                  // Don't use var because its value is globally changing, if the value is changed in one file, then its value is changed in other file. 
+//                                  // Blockscope - Value within the block Example within if statement
+//                                  // Globalscope - Everything else, except block
+// if(true){
+//     let a = 10
+//     const b = 20
 
-    // console.log("INNER :",a)
-}
+//     // console.log("INNER :",a)
+// }
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
     
+// }
+
+// console.log(a);
+// console.log(b);
+// console.log(c) 
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username)
+    }
+   // console.log(website)
+
+   two()
+}
+  one()
+
+  if(true){
+    const username = "hitesh"
+    if(username === "hitesh"){
+        const website =" youtube"
+        console.log(username +website)
+
+    }
+    //console.log(website)
+  }
+  //console.log(username)
+
+ // ++++++++++++++++++++++++++ Interesting +++++++++++++++++++++++++
+
+console.log(addone(5))
+
+ function addone(num){
+    return num+1
 }
 
-console.log(a);
-console.log(b);
-console.log(c) 
+
+addTwo(5)
+const addTwo = function(num){
+    return num+2
+}
 
