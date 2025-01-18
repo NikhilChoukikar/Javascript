8dbe71628dbcd917324330afe4c57afbe9a9d94c
NikhilChoukikar <91651344+NikhilChoukikar@users.noreply.github.com>
2025-01-18 11:26:02 +0000

    promise

--- 09_advance_one/promise.js
+++ 09_advance_one/promise.js
@@ -1,52 +1,107 @@
-const promiseOne = new Promise(function(resolve,reject){
-    setTimeout(function(){
-        console.log('Async task is complete');
-        resolve()
-    },1000)
-})
+// const promiseOne = new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         console.log('Async task is complete');
+//         resolve()
+//     },1000)
+// })
 
-promiseOne.then(function(){
-    console.log("Promise consumed");
-})
+// promiseOne.then(function(){
+//     console.log("Promise consumed");
+// })
 
-new Promise(function(resolve,reject){
-    setTimeout(function(){
-        console.log("Async Task 2");
-        resolve()        
-    },1000)
-}).then(function(){
-    console.log("Async 2 resolved");
-})
+// new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         console.log("Async Task 2");
+//         resolve()        
+//     },1000)
+// }).then(function(){
+//     console.log("Async 2 resolved");
+// })
 
-const promiseThree = new Promise(function(resolve,reject){
-    setTimeout(function(){
-      resolve({username :"Chai", email: "chaiexample.com"}) 
+// const promiseThree = new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//       resolve({username :"Chai", email: "chaiexample.com"}) 
 
-    },1000)
-})
+//     },1000)
+// })
 
-promiseThree.then(function(user){
-    console.log(user);    
-})
+// promiseThree.then(function(user){
+//     console.log(user);    
+// })
 
-const promiseFour = new Promise(function(resolve,reject){
-    setTimeout(function(){
-        let error = false
-        if(!error){
-            resolve({username:"hitesh", password: "123"})
-    } else{
-        reject('ERROR: Something went wrong')
-    }
-},1000)
-})
+// const promiseFour = new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         let error = false
+//         if(!error){
+//             resolve({username:"hitesh", password: "123"})
+//     } else{
+//         reject('ERROR: Something went wrong')
+//     }
+// },1000)
+// })
 
-promiseFour
-.then((user) => {
-    console.log(user);
-    return user.username
+// promiseFour
+// .then((user) => {
+//     console.log(user);
+//     return user.username
     
-}).then((username) => {
-    console.log(username);    
-}).catch(function(error){
-    console.log(error);    
-}).finally(() => console.log("The promise is either resolved or rejected"));
+// }).then((username) => {
+//     console.log(username);    
+// }).catch(function(error){
+//     console.log(error);    
+// }).finally(() => console.log("The promise is either resolved or rejected"));
+
+
+// const promiseFive = new Promise(function(resolve,reject){
+//     setTimeout(function(){
+// let error = false
+// if(!error) {
+//     resolve({username:"javascript", password:"123"})
+// }
+// else{
+//     reject('ERROR: JS went wrong')
+// }
+//     },1000)
+// });
+
+// async function consumePromiseFive(){
+//     try{
+//         const response = await promiseFive
+//         console.log(response);
+//     }
+//     catch(error)
+//     {
+//         console.log(error);
+//     }
+//     }
+
+//     consumePromiseFive()
+
+    // async function getAllUsers(){
+    //     try{
+    //         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+    //         const data = await response.json()
+    //         console.log(data);
+    //     }catch(error){
+    //         console.log("E :",error);
+            
+    //     }
+    // }
+
+    // getAllUsers()
+
+
+
+    // fetch('https://api.github.com/users/nikhilchoukikar')
+    // .then((response) => {
+    //     return response.json()
+    // })
+    // .then((data) => {
+    //     console.log(data);        
+    // })
+    // .catch((error) => console.log(error));
+
+
+    // Promise.all
+    
