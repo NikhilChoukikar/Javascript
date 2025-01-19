5a6091566f678683a4fc4b951cf6a8f4497b1e7c
NikhilChoukikar <91651344+NikhilChoukikar@users.noreply.github.com>
2025-01-19 19:12:46 +0000

    advance object

---
+++ 10_classes_and_oop/mathpi.js
@@ -0,0 +1,42 @@
+// const descriptor = Object.getOwnPropertyDescriptor(Math,"PI")
+
+// console.log(descriptor);
+// Math.PI =5;
+// console.log(Math.PI);
+
+
+// // Object.defineProperty(Math,"PI",{
+// //     writable :false,
+// //     enumerable :true,
+// // })
+
+// console.log(descriptor);
+
+// const chai = {
+//     name : 'ginger chai',
+//     price : 250,
+//     isAvailable : true,
+
+//     orderChai : function(){
+//         console.log("chai nhi bni");
+        
+//     }
+// }
+
+// console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+// Object.defineProperty(chai,'name',{
+//     writable :false,
+//     enumerable :true,
+// })
+
+// console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+// for (let [key,value] of Object.entries(chai)) {
+//     if(typeof value!= 'function'){
+
+//         console.log(`${key} : ${value}`);
+        
+//     }
+    
+// }
